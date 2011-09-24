d7e7d898d527967512693c70ae712006464a4196
schpuntik <schpuntik@freenet.de>
2011-09-24 22:59:49 +0200

    Add scripts for making flashimage or Spark

---
+++ tdt/flash/spark/scripts/flash_part_kernel.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+CURDIR=$1
+TUFSBOXDIR=$2
+OUTDIR=$3
+TMPROOTDIR=$4
+TMPFWDIR=$5
+
+echo "CURDIR       = $CURDIR"
+echo "TUFSBOXDIR   = $TUFSBOXDIR"
+echo "OUTDIR       = $OUTDIR"
+echo "TMPROOTDIR   = $TMPROOTDIR"
+echo "TMPFWDIR     = $TMPFWDIR"
+
+MKFSJFFS2=$TUFSBOXDIR/host/bin/mkfs.jffs2  
+#MUP=$CURDIR/mup
+
+OUTFILE=$OUTDIR/e2jffs2.img 
+
+if [ ! -e $OUTDIR ]; then
+  mkdir $OUTDIR
+fi
+
+if [ -e $OUTFILE ]; then
+  rm -f $OUTFILE
+fi
+
+mv $TMPFWDIR/uImage $OUTDIR/uImage
+
+# Create a kathrein update file for fw's 
+# Offset on NAND Disk = 0x00400000
+$MKFSJFFS2 -l -e0x20000 -n -pv -d $TMPROOTDIR  -o $OUTFILE << EOF
+
+EOF
+
+#$OUTDIR/* zip $OUTFILE.zip $OUTDIR
---
+++ tdt/flash/spark/scripts/prepare_root.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+CURDIR=$1
+RELEASEDIR=$2
+
+TMPROOTDIR=$3
+TMPFWDIR=$4
+
+cp -a $RELEASEDIR/* $TMPROOTDIR
+
+#mv $TMPROOTDIR/boot/uImage $CURDIR/uImage
+
+
+#echo "/dev/mtdblock3	/boot	jffs2	defaults	0	0" >> $TMPROOTDIR/etc/fstab
+#echo "/dev/mtdblock5	/root	jffs2	defaults	0	0" >> $TMPROOTDIR/etc/fstab
+
+
+if [  -e $CURDIR/extras/dev_ufs912.tar.gz ]; then
+  tar -xzf $CURDIR/extras/dev_ufs912.tar.gz -C $TMPROOTDIR/dev/
+fi
+  rm -rf  $TMPROOTDIR/dev/dvb/adapter0/ca1 
+	  cd $TMPROOTDIR/dev/dvb/adapter0 
+	   pwd
+	  ln -s   ca0 $TMPROOTDIR/dev/dvb/adapter0/ca1
