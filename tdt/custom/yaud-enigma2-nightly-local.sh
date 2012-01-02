64b6fff134ba79ee53ed4441a96907565d9d6788
schpuntik <schpuntik@freenet.de>
2012-01-02 23:53:07 +0100

    scripts update

--- tdt/custom/yaud-enigma2-nightly-local.sh
+++ tdt/custom/yaud-enigma2-nightly-local.sh
@@ -2,7 +2,7 @@
 CURDIR=`pwd`
 NRELDIR='../../tufsbox/release'
 CHANGEDIR='../../tufsbox'
-TARGET=`cat $CURDIR/lastChoice | awk -F '--enable-' '{print $5}' | cut -d ' ' -f 1`
+TARGET=`cat $CURDIR/lastChoice | awk -F '--enable-' '{print $7}' | cut -d ' ' -f 1`
 BUILDDIR='../../cvs/cdk'
 
 echo "src/gz AR-P http://amiko.sat-universum.de" | cat - $NRELDIR/etc/opkg/official-feed.conf > $NRELDIR/etc/opkg/official-feed
@@ -19,9 +19,13 @@ echo config.misc.epgcache_filename=/hdd/epg.dat >> $NRELDIR/etc/enigma2/settings
 rm  $NRELDIR/usr/local/share/enigma2/keymap_*.xml
 cp -RP ~/enigma2-amiko/enigma2/* $NRELDIR/
 rm $NRELDIR/lib/modules/pti.ko
-mv $NRELDIR/lib/modules/pti_207.ko $NRELDIR/lib/modules/pti.ko
-#rm $NRELDIR/lib/modules/pti.ko
-#mv $NRELDIR/lib/modules/pti_207.ko $NRELDIR/lib/modules/pti.ko
+if [ $TARGET == p0209 ]; then
+  mv $NRELDIR/lib/modules/pti_209.ko $NRELDIR/lib/modules/pti.ko
+elif [ $TARGET == p0207 ]; then
+    mv $NRELDIR/lib/modules/pti_207.ko $NRELDIR/lib/modules/pti.ko
+elif [ $TARGET == p0123 ]; then
+    mv $NRELDIR/lib/modules/pti_123.ko $NRELDIR/lib/modules/pti.ko
+fi
 exit
 echo "--- Erledigt ---"
 exit
