486509d47c484894c972a17cf16f9b60f968b3a9
Igornord <kengoo@gmail.com>
2012-02-08 13:48:15 +0200

    Add symlink for compiling external plugins

--- tdt/cvs/cdk/make/externe-plugins.mk
+++ tdt/cvs/cdk/make/externe-plugins.mk
@@ -10,7 +10,7 @@ $(DEPDIR)/enigma2-plugins-sh4.do_prepare:
 		git clone git://github.com/schpuntik/enigma2-plugins-sh4.git $(appsdir)/plugins;\
 	fi
 	git clone git://github.com/schpuntik/enigma2-plugins-sh4.git $(appsdir)/plugins
-	cd $(appsdir)/plugins; git checkout master; cd "$(buildprefix)"; \
+	cd $(appsdir)/plugins; ln -s ../../../tufsbox/cdkroot/usr/include/enigma2/lib lib; git checkout master; cd "$(buildprefix)"; \
 	touch $@
 
 $(appsdir)/plugins/config.status:
