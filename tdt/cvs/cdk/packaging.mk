6e615ef81abfcc20c73d2f1f713d272023061572
Alex Maystrenko <alexeytech@gmail.com>
2012-03-08 16:03:48 +0200

    two package targets: cdkflash and cdkroot

--- tdt/cvs/cdk/packaging.mk
+++ tdt/cvs/cdk/packaging.mk
@@ -12,34 +12,63 @@ PKDIR := $(packagingtmpdir)
 
 export packagingtmpdir
 
+ipkcdk := $(prefix)/ipkcdk
+
+$(ipkcdk):
+	$(INSTALL) -d $@
+
 define extra_build
+	rm -rf $(ipkgbuilddir)/*
 	python split_packages.py
-	$(call do_build_pkg,none)
+	$(call do_build_pkg,none,flash)
 endef
 
-define toimage_build
+define toflash_build
+	rm -rf $(ipkgbuilddir)/*
 	python split_packages.py
-	$(call do_build_pkg,install)
+	$(call do_build_pkg,install,flash)
+endef
+
+define tocdk_build
+	rm -rf $(ipkgbuilddir)/*
+	export FILES_$(PARENT_PK)="/"; \
+	python split_packages.py
+	$(call do_build_pkg,install,cdk)
 endef
 
 define do_build_pkg
 	for pkg in `ls $(ipkgbuilddir)`; do \
-		ipkg-build -o root -g root $(ipkgbuilddir)/$$pkg $(ipkprefix) |tee tmpname \
+		ipkg-build -o root -g root $(ipkgbuilddir)/$$pkg $(if $(filter cdk,$(2)),$(ipkcdk),$(ipkprefix)) |tee tmpname \
 		$(if $(filter install,$(1)), && \
 			pkgn=`cat tmpname |perl -ne 'if (m/Packaged contents/) { print ((split / /)[-1])}'` && \
-			ipkg install -f $(crossprefix)/etc/ipkg.conf $$pkgn \
+			ipkg install -f $(crossprefix)/etc/ipkg$(if $(filter cdk,$(2)),-cdk).conf $$pkgn \
 		); done
+#FIXME: too frequent invokes
+	$(if $(filter flash,$(2)),
+		$(prepare_pkginfo_for_flash),
+		$(rewrite_libtool)
+		$(rewrite_pkgconfig)
+	)
 endef
 
 define start_build
 	rm -rf $(PKDIR)
-	rm -rf $(ipkgbuilddir)/*
 	mkdir $(PKDIR)
 endef
 
 define prepare_pkginfo_for_flash
 	perl -pi -e "s,$(flashprefix)/root,," $(flashprefix)/root/usr/lib/ipkg/info/*.list
 endef
+
+define rewrite_libtool
+	 find $(PKDIR) -name "*.la" -type f -exec perl -pi -e "s,^libdir=.*$$,libdir='$(targetprefix)/usr/lib'," {} \;
+endef
+
+define rewrite_pkgconfig
+	 find $(PKDIR) -name "*.pc" -type f -exec perl -pi -e "s,^prefix=.*$$,prefix=$(targetprefix)/usr," {} \;
+endef
+
+
 
 git_version := git log -1 --format=%cd --date=short |sed s/-//g
 get_git_version = $(eval export PKGV_$(PARENT_PK) = $(shell cd $(DIR_$(PARENT_PK)) && $(git_version)))
