dded518dca0c8e9e8a3e49ce1c379eb2785b6443
schpuntik <schpuntik@freenet.de>
2012-04-10 21:49:30 +0200

    local script fixed

--- tdt/custom/yaud-enigma2-pli-nightly-local.sh
+++ tdt/custom/yaud-enigma2-pli-nightly-local.sh
@@ -8,17 +8,17 @@ BUILDDIR='../../cvs/cdk'
 
 echo "src/gz AR-P http://vip.sat-universum.de" | cat - $NRELDIR/etc/opkg/official-feed.conf > $NRELDIR/etc/opkg/official-feed
 mv $NRELDIR/etc/opkg/official-feed $NRELDIR/etc/opkg/official-feed.conf
-echo config.usage.blinking_display_clock_during_recording=true >> $NRELDIR/etc/enigma2/settings
-echo config.usage.e1like_radio_mode=true >> $NRELDIR/etc/enigma2/settings
-echo config.usage.multibouquet=true >> $NRELDIR/etc/enigma2/settings
-echo config.usage.setup_level=expert >> $NRELDIR/etc/enigma2/settings
-echo config.usage.quickzap_bouquet_change=true >> $NRELDIR/etc/enigma2/settings
-echo config.usage.multiepg_ask_bouquet=true >> $NRELDIR/etc/enigma2/settings
-echo config.skin.primary_skin=AmikoSkin/skin.xml >> $NRELDIR/etc/enigma2/settings
-echo config.audio.volume=30 >> $NRELDIR/etc/enigma2/settings
-echo config.misc.epgcache_filename=/hdd/epg.dat >> $NRELDIR/etc/enigma2/settings
+# echo config.usage.blinking_display_clock_during_recording=true >> $NRELDIR/etc/enigma2/settings
+# echo config.usage.e1like_radio_mode=true >> $NRELDIR/etc/enigma2/settings
+# echo config.usage.multibouquet=true >> $NRELDIR/etc/enigma2/settings
+# echo config.usage.setup_level=expert >> $NRELDIR/etc/enigma2/settings
+# echo config.usage.quickzap_bouquet_change=true >> $NRELDIR/etc/enigma2/settings
+# echo config.usage.multiepg_ask_bouquet=true >> $NRELDIR/etc/enigma2/settings
+# echo config.skin.primary_skin=AmikoSkin/skin.xml >> $NRELDIR/etc/enigma2/settings
+# echo config.audio.volume=30 >> $NRELDIR/etc/enigma2/settings
+# echo config.misc.epgcache_filename=/hdd/epg.dat >> $NRELDIR/etc/enigma2/settings
 rm  $NRELDIR/usr/local/share/enigma2/keymap_*.xml
-cp -RP ~/enigma2-amiko/enigma2/* $NRELDIR/
+cp -RP ~/enigma2-amiko/enigma2-pli/* $NRELDIR/
 rm $NRELDIR/lib/modules/pti.ko
 if [ $TARGET == p0209 ]; [ $DIST == spark ]; then
   mv $NRELDIR/lib/modules/pti_209.ko $NRELDIR/lib/modules/pti.ko
