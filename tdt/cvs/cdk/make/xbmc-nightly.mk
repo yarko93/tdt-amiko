60a5fa9501d53503d8ac8d8e5eef21153f34a7c1
Schischu <schischu@duckbox>
2012-05-16 20:25:15 +0200

    [xbmc] fribidi version compatibility

--- tdt/cvs/cdk/make/xbmc-nightly.mk
+++ tdt/cvs/cdk/make/xbmc-nightly.mk
@@ -28,6 +28,7 @@ $(DEPDIR)/xbmc-nightly.do_prepare:
 	[ "$$REVISION" == "" ] || (cd $(appsdir)/xbmc-nightly; git checkout "$$REVISION"; cd "$(buildprefix)";); \
 	cp -ra $(appsdir)/xbmc-nightly $(appsdir)/xbmc-nightly.org; \
 	cd $(appsdir)/xbmc-nightly && patch -p1 < "../../cdk/Patches/xbmc-add_sh_arch.patch"; \
+	cd $(appsdir)/xbmc-nightly && patch -p1 < "../../cdk/Patches/xbmc-fribidi_version_compatibility.patch"; \
 	cp -ra $(appsdir)/xbmc-nightly $(appsdir)/xbmc-nightly.patched
 	touch $@
 
