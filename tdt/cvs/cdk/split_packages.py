ef1cf443ceb58ca3629f1c914fafadf7dc271706
Alex Maystrenko <alexeytech@gmail.com>
2012-06-17 12:21:11 +0300

    packaging fix DEPENDS print to control file

--- tdt/cvs/cdk/split_packages.py
+++ tdt/cvs/cdk/split_packages.py
@@ -313,6 +313,8 @@ def write_control_file(fdir, full_package):
 		var = bb_get(ext(l[1][0]))
 		if l[1][0] == 'NAME':
 			var = var.replace('_', '-').lower()
+		elif l[1][0] == 'RDEPENDS':
+			var = var.replace(' ',',')
 		if not var: continue
 		var = l[0] % var
 		s += var
