9ba6b4f0d9b14f348c311a2effdc1c9c2bafe186
Alex Maystrenko <alexeytech@gmail.com>
2012-07-14 00:31:34 +0300

    externe-plugins migrate to smart-rules..

--- tdt/cvs/cdk/make/externe-plugins.mk
+++ tdt/cvs/cdk/make/externe-plugins.mk
@@ -3,30 +3,23 @@
 #
 #
 DESCRIPTION_e2plugin := Additional plugins for Enigma2
-MAINTAINER_e2plugin := Ar-P team
-REPO_e2plugin = git://github.com/schpuntik/enigma2-plugins-sh4.git
-SRC_URI_e2plugin = $(REPO_e2plugin)
-PACKAGE_ARCH_e2plugin := sh4
+
+PKGR_e2plugin = r2
+
 NAME_e2plugin_meta := enigma2-plugins-meta
 FILES_e2plugin_meta := /usr/share/meta
 DESCRIPTION_e2plugin_meta := Enigma2 plugins metadata
 PACKAGES_e2plugin = e2plugin_meta
 
-enigma2-plugins-sh4:
-$(DEPDIR)/enigma2-plugins-sh4.do_prepare:
-	rm -rf $(appsdir)/plugins; \
-	if [ -e $(targetprefix)/usr/bin/enigma2 ]; then \
-		git clone git://github.com/schpuntik/enigma2-plugins-sh4.git $(appsdir)/plugins;\
-	fi
-	git clone git://github.com/schpuntik/enigma2-plugins-sh4.git $(appsdir)/plugins && \
-	cd $(appsdir)/plugins && \
-	git checkout master; cd "$(buildprefix)"; \
+$(DEPDIR)/enigma2-plugins-sh4.do_prepare: @DEPENDS_e2plugin@
+	@PREPARE_e2plugin@
 	touch $@
 
-$(appsdir)/plugins/config.status:
-	cd $(appsdir)/plugins && \
-		./autogen.sh && \
-		sed -e 's|#!/usr/bin/python|#!$(crossprefix)/bin/python|' -i $(appsdir)/plugins/xml2po.py && \
+$(DIR_e2plugin)/config.status: enigma2-plugins-sh4.do_prepare
+	cd $(DIR_e2plugin) && \
+		autoreconf --force --install -I$(hostprefix)/share/aclocal && \
+		sed -e 's|#!/usr/bin/python|#!$(crossprefix)/bin/python|' -i xml2po.py && \
+		$(BUILDENV) \
 		./configure \
 			--host=$(target) \
 			--with-libsdl=no \
@@ -36,29 +29,23 @@ $(appsdir)/plugins/config.status:
 			--sysconfdir=/etc \
 			STAGING_INCDIR=$(hostprefix)/usr/include \
 			STAGING_LIBDIR=$(hostprefix)/usr/lib \
-			PKG_CONFIG=$(hostprefix)/bin/pkg-config \
-			PKG_CONFIG_PATH=$(targetprefix)/usr/lib/pkgconfig \
 			PY_PATH=$(targetprefix)/usr \
 			$(PLATFORM_CPPFLAGS)
 
-$(DEPDIR)/enigma2-plugins-sh4.do_compile: $(appsdir)/plugins/config.status
-	cd $(appsdir)/plugins && \
+$(DEPDIR)/enigma2-plugins-sh4.do_compile: $(DIR_e2plugin)/config.status
+	cd $(DIR_e2plugin) && \
 		$(MAKE) all
 	touch $@
 
-enigma2-plugins-sh4-package: export PKGV_e2plugin = 3.2git$(shell cd $(appsdir)/plugins && git log -1 --format=%cd --date=short |sed s/-//g)
-PKGR_e2plugin = r1
 enigma2_plugindir = /usr/lib/enigma2/python/Plugins
 
-enigma2-plugins-sh4-package: enigma2-plugins-sh4.do_compile
+$(DEPDIR)/enigma2-plugins-sh4: enigma2-plugins-sh4.do_compile
 	$(call parent_pk,e2plugin)
-	rm -rf $(packagingtmpdir)
-	mkdir -p $(packagingtmpdir)
-	$(MAKE) -C $(appsdir)/plugins install DESTDIR=$(packagingtmpdir)
-	rm -rf $(ipkgbuilddir)
-	@echo 'next variables are exported to enviroment:'
-	@echo $(EXPORT_ENV) | tr ' ' '\n'
-	@echo $(enigma2_plugindir)
+	$(start_build)
+	$(MAKE) -C $(DIR_e2plugin) install DESTDIR=$(PKDIR)
+	rm -rf $(ipkgbuilddir)/*
+	$(flash_prebuild)
+
 	echo -e "\
 	from split_packages import * \n\
 	print bb_data \n\
@@ -66,28 +53,18 @@ enigma2-plugins-sh4-package: enigma2-plugins-sh4.do_compile
 	for package in bb_get('PACKAGES').split(): \n\
 		pk = bb_get('NAME_' + package).split('-')[-1] \n\
 		try: \n\
-			read_control_file('$(appsdir)/plugins/' + pk + '/CONTROL/control') \n\
+			read_control_file('$(DIR_e2plugin)' + pk + '/CONTROL/control') \n\
 		except IOError: \n\
 			print 'skipping', pk \n\
 	do_finish() \n\
 	" | $(crossprefix)/bin/python
-	for p in `ls $(ipkgbuilddir)`; do \
-		ipkg-build -o root -g root $(ipkgbuilddir)/$$p $(ipkprefix); \
-	done
 
-$(DEPDIR)/enigma2-plugins-sh4: enigma2-plugins-sh4.do_prepare enigma2-plugins-sh4.do_compile enigma2-plugins-sh4-package
+	$(call do_build_pkg,none,extra)
 	touch $@
 
 enigma2-plugins-sh4-clean:
 	rm -f $(DEPDIR)/enigma2-plugins-sh4
 	rm -f $(DEPDIR)/enigma2-plugins-sh4.do_compile
 
-enigma2-plugins-sh4-distclean:
-	rm -f $(DEPDIR)/enigma2-plugins-sh4
-	rm -f $(DEPDIR)/enigma2-plugins-sh4.do_compile
+enigma2-plugins-sh4-distclean: enigma2-plugins-sh4-clean
 	rm -f $(DEPDIR)/enigma2-plugins-sh4.do_prepare
-	rm -rf $(appsdir)/plugins
-
-enigma2-sh4-package-distclean:
-	rm -rf $(ipkgbuilddir)
-	rm -rf $(ipkprefix)
