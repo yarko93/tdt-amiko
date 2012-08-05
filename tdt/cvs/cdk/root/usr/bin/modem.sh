680a928613a694bb300696f92d75311395b17ffc
andreyua <andrey.wys@gmail.com>
2012-08-06 01:34:12 +0300

    [modem]: add autodetect modem port

--- tdt/cvs/cdk/root/usr/bin/modem.sh
+++ tdt/cvs/cdk/root/usr/bin/modem.sh
@@ -1,36 +1,37 @@
 #!/bin/sh
 
-# echo $1 $2 >> /tmp/modem.log
+# echo $1 $2 $3 $4 >> /tmp/modem.log
 
 . /etc/modem.conf
 
-if [ -z "$MODEMTYPE" ]; then MODEMTYPE="0"; fi
-if [ -z "$MODEMPORT" ]; then MODEMPORT="ttyUSB0"; fi
-if [ -z "$MODEMSPEED" ]; then MODEMSPEED=""; fi
-if [ -z "$APN" ]; then APN="internet"; fi
-if [ -z "$MODEMUSERNAME" ]; then MODEMUSERNAME=""; fi
-if [ -z "$MODEMPASSWORD" ]; then MODEMPASSWORD=""; fi
-if [ -z "$MODEMMTU" ]; then MODEMMTU="1460"; fi
-if [ -z "$MODEMMRU" ]; then MODEMMRU="1460"; fi
-if [ -z "$MODEMPPPDOPTS" ]; then MODEMPPPDOPTS=""; fi
-if [ -z "$DIALNUMBER" ]; then DIALNUMBER="*99#"; fi
-if [ -z "$DISABLEAUTOSTART" ]; then DISABLEAUTOSTART="0"; fi
+[ -z "$MODEMTYPE" ] && MODEMTYPE=0
+[ -z "$MODEMPORT" ] && MODEMPORT="ttyUSB0"
+[ -z "$MODEMSPEED" ] && MODEMSPEED=""
+[ -z "$APN" ] && APN="internet"
+[ -z "$MODEMUSERNAME" ] && MODEMUSERNAME=""
+[ -z "$MODEMPASSWORD" ] && MODEMPASSWORD=""
+[ -z "$MODEMMTU" ] && MODEMMTU="1460"
+[ -z "$MODEMMRU" ] && MODEMMRU="1460"
+[ -z "$MODEMPPPDOPTS" ] && MODEMPPPDOPTS=""
+[ -z "$DIALNUMBER" ] && DIALNUMBER="*99#"
+[ -z "$DISABLEAUTOSTART" ] && DISABLEAUTOSTART="0"
 
+[ $MODEMPORT = "auto" ] &&  MODEMPORT=`cat /etc/modem.list | grep "$3\:$4"|cut -f 3 -d : -s`
+if [ -z $MODEMPORT ]; then
+echo "Unknown modem $3\:$4 Please specify the modem port manually" >> /tmp/modem.log 
+# [ $MODEMTYPE = "0" ] && MODEMPORT=ttyUSB0 || MODEMPORT=ttyACM0
+fi
 [ $2 != $MODEMPORT ] &&  exit 0
 [ $DISABLEAUTOSTART = "1" ] && [ $1 = "add" ] &&  exit 0
 
 killall pppd
-
 rm -rf /etc/ppp/peers/0.chat
 rm -rf /etc/ppp/peers/1.chat
 rm -rf /etc/ppp/peers/dialup
 rm -rf /etc/ppp/resolv.conf
-
 [ $1 = "remove" ] &&  exit 0
 
-
 [ -d /etc/ppp/peers ] || mkdir -p /etc/ppp/peers
-
 echo "ABORT '~'
 ABORT 'BUSY'
 ABORT 'NO CARRIER'
@@ -80,5 +81,4 @@ password $MODEMPASSWORD
 connect \"/sbin/chat -s -S -V -t 60 -f /etc/ppp/peers/$MODEMTYPE.chat 2>/tmp/chat.log\"" > /etc/ppp/peers/dialup
 
 [ -c /dev/ppp ] ||  mknod /dev/ppp c 108 0
-
 pppd call dialup & >> /tmp/chat.log
