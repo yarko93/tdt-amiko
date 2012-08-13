ded4dd3baabceda02a27ead4a25a3311874e5475
Alex Maystrenko <alexeytech@gmail.com>
2012-08-13 22:17:03 +0300

    small remote-reinstall script use simple config

--- tdt/cvs/cdk/remote-reinstall.sh
+++ tdt/cvs/cdk/remote-reinstall.sh
@@ -7,4 +7,9 @@ for x in $ipkdir/$1*; do
   python ipkg-utils-050831/ipkg-update-index $ipkdir $x
 done
 cat $ipkdir/Packages | gzip > $ipkdir/Packages.gz
-./pkg.py $1
+ip=`cat remote-reinstall.conf`
+if test -z $ip; then
+	echo "write your box ip address to remote-reinstall.conf"
+	exit 1
+fi
+./pkg.py $1 $ip
