b7121719840877a0a3d5337ece71a783e25f0993
schpuntik <schpuntik@freenet.de>
2012-08-14 21:56:14 +0200

    add xbmc build

--- tdt/cvs/cdk/make/xbmc-nightly.mk
+++ tdt/cvs/cdk/make/xbmc-nightly.mk
@@ -14,11 +14,13 @@ $(DEPDIR)/xbmc-nightly.do_prepare:
 	echo " 0) Newest (Can fail due to outdated patch)"; \
 	echo "---- REVISIONS ----"; \
 	echo "1) Sat, 14 Apr 2012 12:36 - 460e79416c5cb13010456794f36f89d49d25da75"; \
-	echo "2) current inactive... comming soon, here is the next stable (case 2 == DIFF=2)"; \
+	echo "2) Sun, 10 Jun 2012 13:53 - 327710767d2257dad27e3885effba1d49d4557f0"; \
+	echo "3) current inactive... comming soon, here is the next stable (case 3 == DIFF=3)"; \
 	read -p "Select: "; \
 	echo "Selection: " $$REPLY; \
-	[ "$$REPLY" == "0" ] && DIFF="0"; \
+	[ "$$REPLY" == "0" ] && DIFF="2"; \
 	[ "$$REPLY" == "1" ] && DIFF="1" && REVISION="460e79416c5cb13010456794f36f89d49d25da75"; \
+	[ "$$REPLY" == "2" ] && DIFF="2" && REVISION="327710767d2257dad27e3885effba1d49d4557f0"; \
 	echo "Revision: " $$REVISION; \
 	[ -d "$(appsdir)/xbmc-nightly" ] && \
 	git pull $(appsdir)/xbmc-nightly $$HEAD;\
@@ -27,15 +29,14 @@ $(DEPDIR)/xbmc-nightly.do_prepare:
 	cp -ra $(appsdir)/xbmc-nightly $(appsdir)/xbmc-nightly.newest; \
 	[ "$$REVISION" == "" ] || (cd $(appsdir)/xbmc-nightly; git checkout "$$REVISION"; cd "$(buildprefix)";); \
 	cp -ra $(appsdir)/xbmc-nightly $(appsdir)/xbmc-nightly.org; \
-	cd $(appsdir)/xbmc-nightly && patch -p1 < "../../cdk/Patches/xbmc-add_sh_arch.patch"; \
-	cd $(appsdir)/xbmc-nightly && patch -p1 < "../../cdk/Patches/xbmc-fribidi_version_compatibility.patch"; \
-	cd $(appsdir)/xbmc-nightly && patch -p1 < "../../cdk/Patches/xbmc-remove_mysql_dependency.patch"; \
+	cd $(appsdir)/xbmc-nightly && patch -p1 < "../../cdk/Patches/xbmc-nightly.$$DIFF.diff"; \
 	cp -ra $(appsdir)/xbmc-nightly $(appsdir)/xbmc-nightly.patched
 	touch $@
 
-
+#			PYTHON_LDFLAGS='-L$(targetprefix)/usr/include/python2.6 -lpython2.6' \
+#			PYTHON_VERSION='2.6' \
 #endable webserver else httpapihandler will fail
-$(appsdir)/xbmc-nightly/config.status: bootstrap libboost directfb libass libmpeg2 libmad jpeg libsamplerate libogg libvorbis libmodplug curl libflac bzip2 tiff lzo libz fontconfig libfribidi freetype sqlite libpng libpcre libcdio jasper yajl libmicrohttpd
+$(appsdir)/xbmc-nightly/config.status: bootstrap libboost directfb libstgles libass libmpeg2 libmad jpeg libsamplerate libogg libvorbis libmodplug curl libflac bzip2 tiff lzo libz fontconfig libfribidi freetype sqlite libpng libpcre libcdio jasper yajl libmicrohttpd tinyxml python gstreamer gst_plugins_dvbmediasink expat
 	cd $(appsdir)/xbmc-nightly && \
 		$(BUILDENV) \
 		./bootstrap && \
@@ -44,6 +45,8 @@ $(appsdir)/xbmc-nightly/config.status: bootstrap libboost directfb libass libmpe
 			--prefix=/usr \
 			PKG_CONFIG=$(hostprefix)/bin/pkg-config \
 			PKG_CONFIG_PATH=$(targetprefix)/usr/lib/pkgconfig \
+			PYTHON_SITE_PKG=$(targetprefix)/usr/lib/python2.6/site-packages \
+			PYTHON_CPPFLAGS=-I$(targetprefix)/usr/include/python2.6 \
 			PY_PATH=$(targetprefix)/usr \
 			--disable-gl \
 			--enable-glesv1 \
@@ -51,12 +54,12 @@ $(appsdir)/xbmc-nightly/config.status: bootstrap libboost directfb libass libmpe
 			--disable-sdl \
 			--enable-webserver \
 			--disable-x11 \
-			--disable-directfb \
 			--disable-samba \
 			--disable-mysql \
 			--disable-joystick \
 			--disable-rsxs \
 			--disable-projectm \
+			--disable-goom \
 			--disable-nfs \
 			--disable-afpclient \
 			--disable-airplay \
@@ -69,7 +72,13 @@ $(appsdir)/xbmc-nightly/config.status: bootstrap libboost directfb libass libmpe
 			--disable-texturepacker \
 			--disable-udev \
 			--disable-libusb \
-			--disable-libcec
+			--disable-libcec \
+			--enable-gstreamer \
+			--disable-paplayer \
+			--enable-gstplayer \
+			--enable-dvdplayer \
+			--disable-pulse \
+			--disable-alsa
 
 $(DEPDIR)/xbmc-nightly.do_compile: $(appsdir)/xbmc-nightly/config.status
 	cd $(appsdir)/xbmc-nightly && \
