6be1b15342825305560b20824fe63afb0c450969
Taapat <taapat@gmail.com>
2013-02-13 19:51:44 +0200

    Update to busybox 1.21.0, thx max10

--- tdt/cvs/cdk/make/busybox.mk
+++ tdt/cvs/cdk/make/busybox.mk
@@ -3,13 +3,10 @@
 #
 BEGIN[[
 busybox
-  1.20.2
+  1.21.0
   {PN}-{PV}
   extract:http://www.{PN}.net/downloads/{PN}-{PV}.tar.bz2
-  patch:file://{PN}-{PV}-kernel_ver.patch
-  patch:file://{PN}-{PV}-ntpd.patch
-  patch:file://{PN}-{PV}-pkg-config-selinux.patch
-  patch:file://{PN}-{PV}-sys-resource.patch
+  patch:file://{PN}-{PV}-mdev.patch
   make:install:CONFIG_PREFIX=PKDIR
 ;
 ]]END
@@ -20,8 +17,6 @@ $(DEPDIR)/busybox.do_prepare: $(DEPENDS_busybox)
 
 $(DEPDIR)/busybox.do_compile: bootstrap $(DEPDIR)/busybox.do_prepare Patches/busybox-1.20.2.config | $(DEPDIR)/$(GLIBC_DEV)
 	cd $(DIR_busybox) && \
-		export CROSS_COMPILE=$(target)- && \
-		$(MAKE) mrproper && \
 		$(INSTALL) -m644 ../$(lastword $^) .config && \
 		$(MAKE) all \
 			CROSS_COMPILE=$(target)- \
