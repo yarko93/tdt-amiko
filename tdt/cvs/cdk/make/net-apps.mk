a71c2bad449d6f6c9de12aab83dbf1fce8d26840
andreyua <andrey.wys@gmail.com>
2013-04-19 14:48:33 +0300

    add symlinks for autostart

--- tdt/cvs/cdk/make/net-apps.mk
+++ tdt/cvs/cdk/make/net-apps.mk
@@ -482,7 +482,7 @@ transmission
 ]]END
 
 DESCRIPTION_transmission = "A free, lightweight BitTorrent client"
-PKGR_transmission = r1
+PKGR_transmission = r2
 RDEPENDS_transmission = curl openssl libevent
 FILES_transmission = \
 /usr/bin/* \
@@ -494,7 +494,7 @@ $(DEPDIR)/transmission.do_prepare: $(DEPENDS_transmission)
 	$(PREPARE_transmission)
 	touch $@
 
-$(DEPDIR)/transmission.do_compile: bootstrap libevent-dev $(DEPDIR)/transmission.do_prepare
+$(DEPDIR)/transmission.do_compile: bootstrap libevent-dev openssl-dev curl $(DEPDIR)/transmission.do_prepare
 	cd $(DIR_transmission) && \
 		$(BUILDENV) \
 		./configure \
@@ -524,6 +524,11 @@ $(DEPDIR)/%transmission: $(DEPDIR)/transmission.do_compile
 		$(INSTALL_FILE) $(PKDIR)/../root/etc/transmission.json $(PKDIR)/etc/transmission/settings.json && \
 		$(INSTALL_DIR) $(PKDIR)/etc/init.d
 		$(INSTALL_BIN) $(PKDIR)/../root/etc/init.d/transmission $(PKDIR)/etc/init.d/transmission
+		$(INSTALL_DIR) $(PKDIR)/etc/rc.d
+		$(INSTALL_DIR) $(PKDIR)/etc/rc.d/rcS.d
+		$(INSTALL_DIR) $(PKDIR)/etc/rc.d/rc6.d
+		ln -sf ../init.d/transmission $(PKDIR)/etc/rc.d/rcS.d/S100transmission
+		ln -sf ../init.d/transmission $(PKDIR)/etc/rc.d/rc6.d/K02transmission
 	$(tocdk_build)
 	$(toflash_build)
 #	@DISTCLEANUP_transmission@
